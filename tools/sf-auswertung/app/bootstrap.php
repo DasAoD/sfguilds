803adea3ca3b138a0baad4084a9532caf918446d
DasAoD <42589682+DasAoD@users.noreply.github.com>
2026-01-13 16:53:14 +0100

    Delete tools/sf-auswertung directory

--- tools/sf-auswertung/app/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-declare(strict_types=1);
-
-date_default_timezone_set('Europe/Berlin');
-mb_internal_encoding('UTF-8');
--- tools/sf-auswertung/public/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-declare(strict_types=1);
-
-header('Location: import.php', true, 302);
-exit;
